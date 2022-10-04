5a0117e5dd3ef1bb0dea6ab8c609e5286b1525e2
Nokiel <114947693+NokielCB@users.noreply.github.com>
2022-10-04 20:53:59 +0200

    Update google1.js

--- google1.js
+++ google1.js
@@ -6,22 +6,29 @@ function wyszukaj() {
         window.open("sklep.html").focus();
     } else if (s.value == "Rolnik" || s.value == "rolnik") {
         document.getElementById('odpowiedz').innerHTML = "Rolnik to dziwne stworzenie gra tylko w lige";
+        document.getElementById('odpowiedz2').innerHTML = "";
     } else if (s.value == "Zielone" || s.value == "zielone") {
         document.getElementById('odpowiedz').innerHTML = "Zielone są zielone";
+        document.getElementById('odpowiedz2').innerHTML = "";
         window.open("https://www.youtube.com/watch?v=w4MBjfLqqMw").focus();
     } else if (s.value == "Rolex" || s.value == "rolex") {
         document.getElementById('odpowiedz').innerHTML = "";
         document.getElementById('odpowiedz2').innerHTML = "Co to za nuta nie znam?";
     } else if (s.value == "Paulina" || s.value == "paulina") {
         document.getElementById('odpowiedz').innerHTML = "No Paulina to super osoba i megą się z nią rozmawia <3 tylko często ma zajęte usta";
+        document.getElementById('odpowiedz2').innerHTML = "";
     } else if (s.value == "Karolina" || s.value == "karolina") {
         document.getElementById('odpowiedz').innerHTML = "Karolina bije cugla";
+        document.getElementById('odpowiedz2').innerHTML = "";
     } else if (s.value == "Ania" || s.value == "ania") {
         document.getElementById('odpowiedz').innerHTML = "Ania nie bij mnie więcej prosze";
+        document.getElementById('odpowiedz2').innerHTML = "";
     } else if (s.value == "Cugiel" || s.value == "cugiel") {
         document.getElementById('odpowiedz').innerHTML = "1 Piwo to nie piwo a 2 piwa to już lizanie z leną";
+        document.getElementById('odpowiedz2').innerHTML = "";
     } else {
         document.getElementById('odpowiedz').innerHTML = "Interesujące pytanie jednak nie znam na to opowiedzi";
+        document.getElementById('odpowiedz2').innerHTML = "";
     }
 }
 
